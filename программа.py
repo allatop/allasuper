9c044b7d307e5dd88def5cbe6eff2a7aa3d59359
allatop <lasevka1902@mail.ru>
2022-09-21 12:14:42 +0300

    new

---
+++ программа.py
@@ -0,0 +1,17 @@
+import random
+
+
+count = 0
+lives = 3
+while True:
+    a = random.randint(0,10)
+    b = random.randint(0,10)
+
+    print(F'{a} + {b} = ')
+    answ = int(input())
+    if answ == (a + b) :
+        count +=1
+        print( 'Ответ верный!' )
+    else:
+        lives = lives - 1
+        print ( 'Ответ неверный!' )
