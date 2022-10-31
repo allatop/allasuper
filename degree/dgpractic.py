69a326ca7711e61a15f13fc2de0d2330e6fa2860
allatop <lasevka1902@mail.ru>
2022-11-01 00:11:55 +0300

    new

--- degree/dgpractic.py
+++ degree/dgpractic.py
@@ -1,12 +1,10 @@
-from degree import библ
-
-
+import degree
 def create(*args, **kwargs):
     p = []
-    for i, deg in range(len(args)):
-        p.append(f'point_{i} = {библ.gms(deg)}')
+    for i in range(len(args)):
+        p.append(f'point_{i} = {degree.gms(args[i])}')
     for name, ged in kwargs.items():
-        p.append(f'{name}={библ.gms(ged)}')
+        p.append(f'{name}={degree.gms(ged)}')
     return p
 
 print(create(172.25899161, 321.42304971, 12.697987681352, pole=21.89617856, put_1= 140.85706440))
--- tasks/modul5/task5.4.py
+++ tasks/modul5/task5.4.py
@@ -1,11 +1,6 @@
-a = input()
-b = input()
-c = input()
-d = input()
-e = '0'
-f = a + b + c + d + e
-f = ' '.join(f).split()
 count = 0
+f = input().split()
+
 while len(f) != 1:
     if f[1] > f[0]:
         count +=1
