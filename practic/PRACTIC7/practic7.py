0adaaf43c4fbefdfc5295727889668c94a1ef335
allatop <lasevka1902@mail.ru>
2022-12-21 01:44:48 +0300

    new

--- practic/PRACTIC7/practic7.py
+++ practic/PRACTIC7/practic7.py
@@ -1,4 +1,6 @@
 import csv
+
+
 def get_books(str):
     dict = []
     with open("books.csv", 'r', encoding='UTF-8') as file:
@@ -7,17 +9,20 @@ def get_books(str):
             if str.lower() in row[1].lower():
                 dict.append(row)
     return dict
+
+
 print(get_books('python'))
+
 
 def get_totals(dict):
     cort = []
     for chicl in dict:
-        sym = float(chicl[3])*float(chicl[4])
-        if sym>=500:
+        sym = float(chicl[3]) * float(chicl[4])
+        if sym >= 500:
             cort.append((chicl[0], sym))
         else:
-            cort.append((chicl[0], sym+100))
+            cort.append((chicl[0], sym + 100))
     return cort
+
+
 print(get_totals(get_books('python')))
-
-
--- practic/PRACTIC8/english learn.py
+++ practic/PRACTIC8/english learn.py
@@ -1,2 +1,47 @@
 import pymorphy2
 import translate
+
+
+def learn_english(name, name2):
+    with open(name, "r", encoding='utf-8') as file:
+        tekst = file.read().split()
+        newfile(name2, sort(redactor(tekst)))
+
+
+def redactor(list):
+    for i in range(len(list)):
+        letter = [a for a in list[i] if a.isalpha()]
+        word = "".join(letter)
+        list[i] = infin(word)
+    return list
+
+
+def infin(word):
+    norm = pymorphy2.MorphAnalyzer()
+    return norm.parse(word)[0].normal_form
+
+
+def sort(words):
+    wdict = {}
+    for word in words:
+        wdict[word] = str(words.count(word))
+    if '' in wdict:
+        wdict.pop('')
+    dict = sorted(wdict.values(), reverse=True)
+    sotrdict = {}
+    for i in dict:
+        for k in wdict.keys():
+            if wdict[k] == i:
+                sotrdict[k] = wdict[k]
+    return sotrdict
+
+
+def newfile(name, count):
+    translat = translate.Translator(from_lang='ru', to_lang='en')
+
+    with open(name, 'w', encoding='utf-8') as file:
+        for word in count:
+            file.write(f'{word} | {translat.translate(word)} | {count[word]}\n')
+
+
+learn_english('text.txt', 'translate.txt')
